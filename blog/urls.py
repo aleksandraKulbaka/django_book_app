376b7d234a5a1febadef6cf71e38773a258e96c4
Aleksandra Kulbaka <aleksandrakulbaka@Aleksandras-MacBook-Pro.local>
2021-10-14 17:22:35 +0100

    Created dashboard page and populated it with dummy data. Created a nav bar

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 # if we have about/, both about and about/
 # paths will be redirected to about/
 urlpatterns = [
-    path('', views.home, name="blog-home"),
-    path('about/', views.about, name="blog-about"),
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'AWK',
+        'bookTitle': 'Hobbit',
+        'bookAuthor': 'J.R.R. Tolkien',
+        'review': 5,
+        'date': 'August 20, 2021'
+    },
+    {
+        'author': 'AWK',
+        'bookTitle': 'Lord of the Rings: The Two Towers',
+        'bookAuthor': 'J.R.R. Tolkien',
+        'review': 5,
+        'date': 'September 01, 2021'
+    },
+    {
+        'author': 'AWK',
+        'bookTitle': "Ranger's apprentice: The Ruins of Gorlan",
+        'bookAuthor': 'John Flanagan',
+        'review': 5,
+        'date': 'October 11, 2021'
+    }
+]
 def home(request):
-    return HttpResponse('<h1> Blog home </h1>')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
 
     # Create your views here.
 def about(request):
-    return HttpResponse('<h1> This is Blog About page </h1>')
+    return render(request, 'blog/about.html')
