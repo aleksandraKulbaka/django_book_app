047ef040965ba2844fcb1a76d606b366fae8fea4
Aleksandra Kulbaka <aleksandrakulbaka@Aleksandras-MacBook-Pro.local>
2021-10-19 12:10:08 +0100

    enabled updating and deleting posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from .views import (
     PostListView, 
     PostDetailView, 
     PostCreateView,
-    PostUpdateView
+    PostUpdateView,
+    PostDeleteView
 )
 
 # if we have about/, both about and about/
@@ -13,6 +14,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
