151190eb321f3b0f65e9e635271cb2932b2d063c
Aleksandra Kulbaka <aleksandrakulbaka@Aleksandras-MacBook-Pro.local>
2021-10-20 16:57:45 +0100

    enabled review score to be from 0 to 5

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 class Post(models.Model):
     bookTitle = models.TextField()
     bookAuthor = models.TextField()
-    review = models.FloatField()
+    review = models.IntegerField(default=0,validators=[MinValueValidator(0), MaxValueValidator(5)])
     date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     public =  models.BooleanField(default=True)
